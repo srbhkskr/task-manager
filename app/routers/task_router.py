61106cd6a74ac77619431f93884419080ddbf090
SK <srbhkskr@gmail.com>
2024-10-13 21:48:28 +0530

    refactor: add logs

--- app/routers/task_router.py
+++ app/routers/task_router.py
@@ -14,7 +14,7 @@ logger = logging.getLogger(__name__)
 
 @router.post("/")
 def create_task(task: PostTaskRequest, db: Session = Depends(get_db)):
-    logger.info(f"got request with {task}")
+    logger.info(f"got create task request with {task}")
     task_entity = Task()
     task_entity.title = task.title
     task_entity.description = task.description
@@ -25,10 +25,12 @@ def create_task(task: PostTaskRequest, db: Session = Depends(get_db)):
 
 @router.get("/")
 def get_tasks(db: Session = Depends(get_db)):
+    logger.info(f"got request to get all tasks")
     return db.query(Task).all()
 
 @router.put("/{task_id}")
 def update_task(task_id: int, updated_task: UpdateTaskRequest, db: Session = Depends(get_db)):
+    logger.info(f"got request to update task {task_id} with {updated_task}")
 
     task_from_db = db.query(Task).where(Task.id.match(task_id)).first()
     if updated_task.title:
@@ -41,14 +43,13 @@ def update_task(task_id: int, updated_task: UpdateTaskRequest, db: Session = Dep
     db.merge(task_from_db)
     db.commit()
     db.refresh(task_from_db)
-    logger.info("task updated")
+    logger.info(f"task {task_id} updated {task_from_db}")
     return task_from_db
 
 @router.delete("/{task_id}")
 def delete_task(task_id: int, db: Session = Depends(get_db)):
-
+    logger.info(f"Got request to delete task {task_id}")
     task_from_db = db.query(Task).where(Task.id.match(task_id)).first()
-
 
     if not task_from_db:
         return
@@ -56,5 +57,5 @@ def delete_task(task_id: int, db: Session = Depends(get_db)):
     db.delete(task_from_db)
     db.commit()
 
-    logger.info("task deleted")
+    logger.info(f"task {task_id} deleted")
     return
