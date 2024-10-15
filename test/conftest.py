0a727d6b235b4d8905fc0872aad82d268085c801
SK <srbhkskr@gmail.com>
2024-10-15 10:18:30 +0530

    fix: create database before test

--- test/conftest.py
+++ test/conftest.py
@@ -1,16 +1,33 @@
 import os
+from sqlite3 import ProgrammingError
 
 import pytest
 from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
 
 from app.db.base import Base, get_db
 from app.main import app
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:root@127.0.0.1/test_task_manager_db")
 
-engine = create_engine(DATABASE_URL)
+# Set up a test database URL
+ADMIN_DATABASE_URL = os.getenv("ADMIN_DATABASE_URL", "postgresql://postgres:root@127.0.0.1/postgres")
+TEST_DATABASE_URL = os.getenv("TEST_DATABASE_URL", "postgresql://postgres:root@127.0.0.1/test_task_manager_db")
+
+admin_engine = create_engine(ADMIN_DATABASE_URL, isolation_level="AUTOCOMMIT")
+
+def create_test_database():
+    """Create the test database if it doesn't exist."""
+    with admin_engine.connect() as connection:
+        try:
+            connection.execute(
+                text(f"CREATE DATABASE {TEST_DATABASE_URL.split('/')[-1]}")
+            )
+        except ProgrammingError as pe:
+            print(pe)
+            print("Database already exists, continuing...")
+
+engine = create_engine(TEST_DATABASE_URL)
 TestSessionLocal = sessionmaker(autocommit=False, autoflush=True, bind=engine)
 
 def override_get_db():
@@ -23,6 +40,7 @@ def override_get_db():
 
 @pytest.fixture()
 def test_db():
+    create_test_database()
     Base.metadata.create_all(bind=engine)
     yield
     Base.metadata.drop_all(bind=engine)
